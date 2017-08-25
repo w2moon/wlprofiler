135f0928812322fc103c7d242f314ec38c5a098d
w2moon <w2moon@gmail.com>
2017-08-25 20:43:00 +0800

    dfd3

--- src/widget/Chart.ts
+++ src/widget/Chart.ts
@@ -7,15 +7,28 @@ interface LineMap {
 }
 export class Chart extends Director{
     lines:LineMap = {};
-    constructor(canvas:HTMLCanvasElement){
+    xPixel:number;
+    yPixel:number;
+    constructor(canvas:HTMLCanvasElement,xPixel:number = 1,yPixel:number = 60){
         super(canvas);
+
+        this.xPixel = xPixel
+
     }
 
     drawLine(name:string,pts:Array<point>){
+        
+
         let line = this.lines[name];
         if(!line){
             line = new Line(pts);
             this.lines[name] = line;
         }
+
+        let width = this.canvas.width;
+        let height = this.canvas.height;
+        pts.forEach(p=>{
+            if(p.)
+        })
     }
 }
